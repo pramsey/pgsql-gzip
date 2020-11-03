35d01b943d90078ad89984a6bf33e5a3cdf03a67
Paul Ramsey <pramsey@cleverelephant.ca>
2020-11-02 16:39:10 -0800

    test

--- ci/ubuntu-install.sh
+++ ci/ubuntu-install.sh
@@ -7,6 +7,6 @@ set -x
 curl "https://salsa.debian.org/postgresql/postgresql-common/-/raw/master/pgdg/apt.postgresql.org.sh" \
 	> /usr/local/bin/apt.postgresql.org.sh
 chmod 755 /usr/local/bin/apt.postgresql.org.sh
-apt.postgresql.org.sh -i -v $PGVERSION
+/usr/local/bin/apt.postgresql.org.sh -i -v $PGVERSION
 pg_createcluster --start $PGVERSION test -p 5432 -- -A trust
 
