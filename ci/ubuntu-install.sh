1f1ab933b71f0e97dd0d12df0015a15cadefcdd1
Paul Ramsey <pramsey@cleverelephant.ca>
2020-11-02 19:45:36 -0800

    test

--- ci/ubuntu-install.sh
+++ ci/ubuntu-install.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-PGVERSION=$1
+PG_VERSION=$1
+UBUNTU=bionic
 
 set -x
 
-curl "https://salsa.debian.org/postgresql/postgresql-common/-/raw/master/pgdg/apt.postgresql.org.sh" \
-	> /usr/local/bin/apt.postgresql.org.sh
-chmod 755 /usr/local/bin/apt.postgresql.org.sh
+/etc/init.d/postgresql stop
 
-for VER in 10 11 12 13; do
-
-	apt-get -y --purge remove postgresql-$VER postgresql-server-dev-$VER
-
+for VER in 9.1 9.2 9.3 9.4 9.5 9.6 10 11 12 13; do
+	apt-get -y --purge remove postgresql-9.1
 done
 
 apt-get -y --purge remove postgresql-common
+apt-get -y autoremove
+rm -rf /var/lib/postgresql
 
-/usr/local/bin/apt.postgresql.org.sh -i -v $PGVERSION
-
-pg_createcluster $PGVERSION main -p 5432 -- -A trust
-pg_ctlcluster $PGVERSION main start
+apt-key adv --fetch-keys https://www.postgresql.org/media/keys/ACCC4CF8.asc
+add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ ${UBUNTU}-pgdg main ${PG_VERSION}"
+apt-get update -qq
+apt-get -y install postgresql-${PG_VERSION} postgresql-server-dev-${PG_VERSION}
+cp ci/pg_hba.conf /etc/postgresql/$PG_VERSION/main/pg_hba.conf
+/etc/init.d/postgresql reload
